44e5e1666ba967862404920c019ab1905c460cf3
Andrew Patasik <andrew.yehezkiel17@gmail.com>
2022-06-23 20:06:13 +0800

    add widget component view

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -20,12 +20,22 @@ const TaskList = ({ taskCollection }) => {
   };
 
   return (
-    <section className="my-8">
+    <section className="mt-8 max-h-96">
       <ul
         id="todo-list"
         className="bg-slate-700 divide-y divide-black rounded-lg"
       >
         {renderTodoTask()}
+        <section className="flex justify-between items-center text-xs font-bold text-slate-500 p-4">
+          <p>{taskCollection.length} items left</p>
+          <article className="flex justify-evenly gap-2">
+            {/* this is filter */}
+            <div>All</div>
+            <div>Active</div>
+            <div>Completed</div>
+          </article>
+          <button>Clear completed</button>
+        </section>
       </ul>
     </section>
   );
